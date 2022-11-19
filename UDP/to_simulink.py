099bde0ce832db5d0f46aa659fcc2a040b1e6eec
Rushidave10 <rushi.1996dave@gmail.com>
2022-11-19 12:01:04 +0100

    weights going back and forth

--- UDP/to_simulink.py
+++ UDP/to_simulink.py
@@ -57,26 +57,29 @@ socket_3 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 counter = 0
 # while counter <= 0:
 
-# msg_port1021 = weights_and_bias('layer1').numpy()
-# msg_port1022 = weights_and_bias('layer2').numpy()
-msg_port1023 = weights_and_bias('layer3').numpy()
+"""Actual Neural Networks weights from pytorch based model"""
+msg_port1021 = weights_and_bias('layer1').numpy().astype('double')
+msg_port1022 = weights_and_bias('layer2').numpy().astype('double')
+msg_port1023 = weights_and_bias('layer3').numpy().astype('double')
 
-msg_port1021 = np.arange(0, 572).reshape(22, 26)
-msg_port1022 = np.arange(0, 650).reshape(26, 25)
-# msg_port1023 = np.arange(0, 78).reshape(26, 3)
+""" To check the sequence of the data that is delivered"""
+# msg_port1021 = np.arange(0, 572, dtype=np.double).reshape(22, 26)
+# msg_port1022 = np.arange(0, 650, dtype=np.double).reshape(26, 25)
+# msg_port1023 = np.arange(0, 78, dtype=np.double).reshape(26, 3)
 
 # msg = bytes('Hello! Welcome to my server', "utf-8")
 # msg = bytes('Hello! ', "utf-8")
 # msg = bytes('bye', 'utf-8')
 
-socket_1.sendto(msg_port1021, address_port_1021)
-socket_2.sendto(msg_port1022, address_port_1022)
-socket_3.sendto(msg_port1023, address_port_1023)
+while True:
+    socket_1.sendto(msg_port1021, address_port_1021)
+    socket_2.sendto(msg_port1022, address_port_1022)
+    socket_3.sendto(msg_port1023, address_port_1023)
 
-print(f'The size of message to port 1021 is {getsizeof(msg_port1021)} Bytes')
-print(f'The size of message to port 1022 is {getsizeof(msg_port1022)} Bytes')
-print(f'The size of message to port 1023 is {getsizeof(msg_port1023)} Bytes')
-print("\n +++++++++++++++++ \n ")
+    # print(f'The size of message to port 1021 is {getsizeof(msg_port1021)} Bytes')
+    # print(f'The size of message to port 1022 is {getsizeof(msg_port1022)} Bytes')
+    # print(f'The size of message to port 1023 is {getsizeof(msg_port1023)} Bytes')
+    # print("\n +++++++++++++++++ \n ")
 
-counter += 1
-# socket_1.sendto(msg, ('131.234.124.101', 25000))
+    counter += 1
+    # socket_1.sendto(msg, ('131.234.124.101', 25000))
