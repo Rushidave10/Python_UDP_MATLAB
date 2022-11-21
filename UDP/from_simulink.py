7da848b0756baa5c77d3b721053a4d61e44c22a7
Rushidave10 <rushi.1996dave@gmail.com>
2022-11-21 10:15:01 +0100

    Improved Validation setup

--- UDP/from_simulink.py
+++ UDP/from_simulink.py
@@ -6,20 +6,21 @@ import numpy as np
 SIZE = 1500
 hostname = socket.gethostname()
 ip_address = socket.gethostbyname(hostname)
-port = 1021
+port = 1111
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.bind((hostname, 1112))
+s.bind((hostname, 1111))
 # print(socket_1.gettimeout())
 # socket_1.settimeout(5.0)
 # print(socket_1.gettimeout())
 
 counter = 0
 
-while counter <= 0:
-
+while True:
+    input("Press Enter to continue")
     print('Sending data')
-    update = np.array([300], dtype=np.double)
-    s.sendto(update, ('131.234.124.101', 1111))
+    update = np.array([counter], dtype=np.double)
+    s.sendto(update, ('131.234.124.101', 1112))
+    print(counter)
 
     print('Waiting for message-----')
     msg_from_simulink = s.recvfrom(SIZE)
