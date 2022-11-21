9a26ae4ea1f5e24c6dc284c46ca2d7ab3afc702e
Rushidave10 <rushi.1996dave@gmail.com>
2022-11-21 11:04:05 +0100

    Improved Validation setup with Multiport

--- UDP/to_simulink.py
+++ UDP/to_simulink.py
@@ -1,10 +1,8 @@
 import socket
 import struct
+import sched, time
 import numpy as np
-import codecs
-import pickle
 from sys import getsizeof
-import torch
 import torch.nn as nn
 
 
@@ -42,6 +40,7 @@ def weights_and_bias(layer, bias=False):
     return model.state_dict()[f'{layer}.weight']
 
 
+'''Need when acting as server'''
 hostname = socket.gethostname()
 ip_address = socket.gethostbyname(hostname)
 
@@ -49,37 +48,45 @@ address_port_1021 = (f'{ip_address}', 1021)
 address_port_1022 = (f'{ip_address}', 1022)
 address_port_1023 = (f'{ip_address}', 1023)
 
+'''Need when receiving'''
+buffer_size = 650000
+
 socket_1 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 socket_2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 socket_3 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-# socket_1.bind((hostname, 1234))
+socket_update = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+socket_update.bind((hostname, 1020))
 counter = 0
-# while counter <= 0:
 
 """Actual Neural Networks weights from pytorch based model"""
-msg_port1021 = weights_and_bias('layer1').numpy().astype('double')
-msg_port1022 = weights_and_bias('layer2').numpy().astype('double')
-msg_port1023 = weights_and_bias('layer3').numpy().astype('double')
+msg_port1021 = weights_and_bias('layer1').numpy().astype('longdouble')
+msg_port1022 = weights_and_bias('layer2').numpy().astype('longdouble')
+msg_port1023 = weights_and_bias('layer3').numpy().astype('longdouble')
 
 """ To check the sequence of the data that is delivered"""
 # msg_port1021 = np.arange(0, 572, dtype=np.double).reshape(22, 26)
 # msg_port1022 = np.arange(0, 650, dtype=np.double).reshape(26, 25)
 # msg_port1023 = np.arange(0, 78, dtype=np.double).reshape(26, 3)
 
-# msg = bytes('Hello! Welcome to my server', "utf-8")
-# msg = bytes('Hello! ', "utf-8")
-# msg = bytes('bye', 'utf-8')
 
-while True:
+for i in range(11):
+    # input('Press Enter to continue')
+    print('Sending Data....to simulink ---->')
     socket_1.sendto(msg_port1021, address_port_1021)
     socket_2.sendto(msg_port1022, address_port_1022)
     socket_3.sendto(msg_port1023, address_port_1023)
 
-    # print(f'The size of message to port 1021 is {getsizeof(msg_port1021)} Bytes')
-    # print(f'The size of message to port 1022 is {getsizeof(msg_port1022)} Bytes')
-    # print(f'The size of message to port 1023 is {getsizeof(msg_port1023)} Bytes')
-    # print("\n +++++++++++++++++ \n ")
+    print(f'The size of message to port 1021 is {getsizeof(msg_port1021)} Bytes')
+    print(f'The size of message to port 1022 is {getsizeof(msg_port1022)} Bytes')
+    print(f'The size of message to port 1023 is {getsizeof(msg_port1023)} Bytes')
+    print("\n +++++++++++++++++ \n ")
 
+    msg_from_simulink = socket_update.recvfrom(buffer_size)
+    update_flag = struct.unpack('d', msg_from_simulink[0])
+    print(type(update_flag))
+    print('The Update flag is :', update_flag)
     counter += 1
-    # socket_1.sendto(msg, ('131.234.124.101', 25000))
+    print('Counter:', counter)
+    ip_address_simulink = msg_from_simulink[1]
+
